ee2ac7c490b1018fcd31cde8d93807e33a96fe8a
Edward Lim <ed.lim199@gmail.com>
2026-02-19 17:28:55 +1100

    tests are passing

--- internal/databases/influxdb/influxdb_test.go
+++ internal/databases/influxdb/influxdb_test.go
@@ -38,12 +38,23 @@ func dir(envFile string) string {
 }
 
 func compareSystemStatusDatapoint(a shared.SystemStatusDatapoint, b map[string]any) bool {
-	// this may be incorrect
-	return a.HDDBytesFree == b["HDDBytesFree"] && a.ProductsPerSecond == b["ProductsPerSecond"] && a.RAMUtilisationPercent == b["RamUtilisationPerSecond"] && a.TotalProductCount == b["TotalProductCount"]
+	ram_utilisation_percentage, ok := b["ram_utilisation_percentage"].(float64)
+	var ram_utilisation_percentageMatch bool = (ok && a.RAMUtilisationPercent == float64(ram_utilisation_percentage)) || (!ok && a.RAMUtilisationPercent == 0 && b["ram_utilisation_percentage"] == nil)
+
+	products_per_second, ok := b["products_per_second"].(float64)
+	var products_per_secondMatch bool = (ok && a.ProductsPerSecond == float64(products_per_second) || (!ok && a.ProductsPerSecond == 0 && b["products_per_second"] == nil))
+
+	hdd_bytes_free, ok := b["hdd_bytes_free"].(int64)
+	var hdd_bytes_freeMatch bool = (ok && a.HDDBytesFree == int(hdd_bytes_free)) || (!ok && a.HDDBytesFree == 0 && b["hdd_bytes_free"] == nil)
+
+	total_product_count, ok := b["total_product_count"].(int64)
+	var total_product_countMatch bool = (ok && int(a.TotalProductCount) == int(total_product_count)) || (!ok && a.TotalProductCount == 0 && b["total_product_count"] == nil)
+
+	return ram_utilisation_percentageMatch && products_per_secondMatch && hdd_bytes_freeMatch && total_product_countMatch
+	// return (a.HDDBytesFree == b["hdd_bytes_free"]) && (a.ProductsPerSecond == b["products_per_second"]) && (a.RAMUtilisationPercent == b["ram_utilisation_percentage"]) && (a.TotalProductCount == b["total_product_count"])
 }
 
 func compareProductInfo(a shared.ProductInfo, b map[string]any) bool {
-	// do type conversion
 	cents, ok := b["cents"].(int64)
 	var centsMatch bool = (ok && a.PriceCents == int(cents)) || (!ok && a.PriceCents == 0 && b["cents"] == nil)
 
@@ -58,8 +69,12 @@ func compareProductInfo(a shared.ProductInfo, b map[string]any) bool {
 }
 
 func compareArbitrarySystemStatusDatapoint(field string, value interface{}, b map[string]any) bool {
-	return b[field] == value
+	// depending on what the type of value is
+	// field is something measurable: integer, float, boolean, string
+
+	return (value == nil && b[field] == nil) || (value == b[field])
 }
+
 func TestWriteProductDatapoint(t *testing.T) {
 	i := InfluxDB{}
 	// read in .env.test
@@ -144,6 +159,7 @@ func TestWriteProductDatapoint(t *testing.T) {
 		result := iterator.Value()
 		if it > 2 {
 			t.Errorf("cardinality of query didn't match what was expected: %d", it)
+			t.FailNow()
 		}
 		if !compareProductInfo(inputPoints[it], result) {
 			t.Errorf("data points don't match what was expected")
@@ -158,137 +174,143 @@ func TestWriteProductDatapoint(t *testing.T) {
 
 }
 
-// func TestWriteSystemDatapoint(t *testing.T) {
-// 	i := InfluxDB{}
-// 	// read in .env.test
-// 	godotenv.Load(dir(".env.test"))
-// 	influxDBUrl, influxDBToken, influxDBDatabase, influxDBSystemTable := os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"), os.Getenv("INFLUXDB_DATABASE"), os.Getenv("INFLUXDB_SYSTEM_TABLE")
-// 	err := i.Init(influxDBUrl, influxDBToken, influxDBDatabase) // have to make an influxdb3 instance for testing
-// 	/*
-// 		install influxdb3 needs to be done manually
-// 		./influxdb3 serve
-// 			--node-id=node0 \
-// 			--cluster-id=cluster0 \
-// 			--object-store=file \
-// 			--data-dir=./data
-// 		install the cli manually
-// 		use the cli to healthcheck
-//
-// 		init a database (perhaps needing to tear down an existing database from previous test run)
-// 	*/
-//
-// 	if err != nil {
-// 		t.Errorf("Could not init database client: %s", err.Error())
-// 		t.FailNow()
-// 	}
-//
-// 	// get the current time
-// 	preWriteTime := time.Now().String()
-//
-// 	// make the input system status points
-// 	inputPoints := make([]shared.SystemStatusDatapoint, 3)
-// 	inputPoints[0] = shared.SystemStatusDatapoint{
-// 		RAMUtilisationPercent: 35.3,
-// 		ProductsPerSecond:     0.05,
-// 		HDDBytesFree:          12,
-// 		TotalProductCount:     12,
-// 	}
-// 	inputPoints[1] = shared.SystemStatusDatapoint{
-// 		RAMUtilisationPercent: 37.30,
-// 		ProductsPerSecond:     0.15,
-// 		HDDBytesFree:          10,
-// 		TotalProductCount:     8,
-// 	}
-// 	inputPoints[2] = shared.SystemStatusDatapoint{
-// 		RAMUtilisationPercent: 12.15,
-// 		ProductsPerSecond:     9.63,
-// 		HDDBytesFree:          10,
-// 		TotalProductCount:     8,
-// 	}
-// 	// write the input product points
-// 	for _, v := range inputPoints {
-// 		i.WriteSystemDatapoint(v)
-// 	}
-//
-// 	// sanity testing: check that only the measurements we wrote exist after preWriteTime (cardinality)
-// 	ctx := context.Background()
-// 	query := fmt.Sprintf("SELECT * FROM %s WHERE time >= TIMESTAMP '%s' ORDER BY time;", influxDBSystemTable, preWriteTime)
-// 	iterator, err := i.db.Query(ctx, query) // not using public interface of InfluxDB, this is not black box testing but in order to keep the interface of the package consistent it's acceptable
-// 	if err != nil {
-// 		t.Errorf("couldn't get query: %s", err.Error())
-// 		t.FailNow()
-// 	}
-// 	var it int = 0
-// 	for iterator.Next() {
-// 		// compare the values to what we wrote and ensure that only 3 exist
-// 		result := iterator.Value()
-// 		compareSystemStatusDatapoint(inputPoints[it], result)
-// 		it++
-// 	}
-// 	if it != 3 {
-// 		t.FailNow()
-// 	}
-//
-// }
-//
-// func TestWriteArbitrarySystemDatapoint(t *testing.T) {
-// 	i := InfluxDB{}
-// 	// read in .env.test
-// 	godotenv.Load(dir(".env.test"))
-// 	influxDBUrl, influxDBToken, influxDBDatabase, influxDBSystemTable := os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"), os.Getenv("INFLUXDB_DATABASE"), os.Getenv("INFLUXDB_SYSTEM_TABLE")
-// 	err := i.Init(influxDBUrl, influxDBToken, influxDBDatabase) // have to make an influxdb3 instance for testing
-// 	/*
-// 		install influxdb3 needs to be done manually
-// 		./influxdb3 serve
-// 			--node-id=node0 \
-// 			--cluster-id=cluster0 \
-// 			--object-store=file \
-// 			--data-dir=./data
-// 		install the cli manually
-// 		use the cli to healthcheck
-//
-// 		init a database (perhaps needing to tear down an existing database from previous test run)
-// 	*/
-//
-// 	if err != nil {
-// 		t.Errorf("Could not init database client: %s", err.Error())
-// 		t.FailNow()
-// 	}
-//
-// 	// get the current time
-// 	preWriteTime := time.Now().String()
-//
-// 	// write the arbitrary system points
-// 	i.WriteArbitrarySystemDatapoint("colour", "grey")
-// 	i.WriteArbitrarySystemDatapoint("number", 42)
-// 	i.WriteArbitrarySystemDatapoint("metres", 1.5)
-//
-// 	// sanity testing: check that only the measurements we wrote exist after preWriteTime (cardinality)
-// 	ctx := context.Background()
-// 	// idk how this is even queried, this links to the original question, are they all being written to the same table
-// 	query := fmt.Sprintf("SELECT * FROM %s WHERE time >= TIMESTAMP '%s' ORDER BY time;", influxDBSystemTable, preWriteTime)
-// 	iterator, err := i.db.Query(ctx, query) // not using public interface of InfluxDB, this is not black box testing but in order to keep the interface of the package consistent it's acceptable
-// 	if err != nil {
-// 		t.Errorf("couldn't get query: %s", err.Error())
-// 		t.FailNow()
-// 	}
-// 	var it int = 0
-// 	for iterator.Next() {
-// 		// compare the values to what we wrote and ensure that only 3 exist
-// 		result := iterator.Value()
-// 		switch it {
-// 		case 0:
-// 			compareArbitrarySystemStatusDatapoint("colour", "grey", result)
-// 		case 1:
-// 			compareArbitrarySystemStatusDatapoint("number", 42, result)
-// 		case 2:
-// 			compareArbitrarySystemStatusDatapoint("metres", 1.5, result)
-// 		}
-// 		it++
-// 	}
-// 	if it != 3 {
-// 		t.FailNow()
-// 	}
-//
-// }
-//
+func TestWriteSystemDatapoint(t *testing.T) {
+	i := InfluxDB{}
+	// read in .env.test
+	godotenv.Load(dir(".env.test"))
+	influxDBUrl, influxDBToken, influxDBDatabase, influxDBSystemTable := os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"), os.Getenv("INFLUXDB_DATABASE"), os.Getenv("INFLUXDB_SYSTEM_TABLE")
+	err := i.Init(influxDBUrl, influxDBToken, influxDBDatabase) // have to make an influxdb3 instance for testing
+	/*
+		install influxdb3 needs to be done manually
+		./influxdb3 serve
+			--node-id=node0 \
+			--cluster-id=cluster0 \
+			--object-store=file \
+			--data-dir=./data
+		install the cli manually
+		use the cli to healthcheck
+
+		init a database (perhaps needing to tear down an existing database from previous test run)
+	*/
+
+	if err != nil {
+		t.Errorf("Could not init database client: %s", err.Error())
+		t.FailNow()
+	}
+
+	// get the current time
+	preWriteTime := time.Now().Format(time.RFC3339Nano)
+
+	// make the input system status points
+	inputPoints := make([]shared.SystemStatusDatapoint, 3)
+	inputPoints[0] = shared.SystemStatusDatapoint{
+		RAMUtilisationPercent: 35.3,
+		ProductsPerSecond:     0.05,
+		HDDBytesFree:          12,
+		TotalProductCount:     12,
+	}
+	inputPoints[1] = shared.SystemStatusDatapoint{
+		RAMUtilisationPercent: 37.30,
+		ProductsPerSecond:     0.15,
+		HDDBytesFree:          10,
+		TotalProductCount:     8,
+	}
+	inputPoints[2] = shared.SystemStatusDatapoint{
+		RAMUtilisationPercent: 12.15,
+		ProductsPerSecond:     9.63,
+		HDDBytesFree:          10,
+		TotalProductCount:     8,
+	}
+	// write the input product points
+	for _, v := range inputPoints {
+		i.WriteSystemDatapoint(v)
+	}
+
+	// sanity testing: check that only the measurements we wrote exist after preWriteTime (cardinality)
+	ctx := context.Background()
+	query := fmt.Sprintf("SELECT * FROM %s WHERE time >= TIMESTAMP '%s' ORDER BY time;", influxDBSystemTable, preWriteTime)
+	iterator, err := i.db.Query(ctx, query) // not using public interface of InfluxDB, this is not black box testing but in order to keep the interface of the package consistent it's acceptable
+	if err != nil {
+		t.Errorf("couldn't get query: %s", err.Error())
+		t.FailNow()
+	}
+	var it int = 0
+	for iterator.Next() {
+		// compare the values to what we wrote and ensure that only 3 exist
+		result := iterator.Value()
+		if it > 2 {
+			t.Errorf("cardinality of query didn't match what was expected: %d", it)
+			t.FailNow()
+		}
+		if !compareSystemStatusDatapoint(inputPoints[it], result) {
+			t.Errorf("data points don't match what was expected")
+			t.FailNow()
+		}
+		it++
+	}
+	if it != 3 {
+		t.FailNow()
+	}
+
+}
+
+func TestWriteArbitrarySystemDatapoint(t *testing.T) {
+	i := InfluxDB{}
+	// read in .env.test
+	godotenv.Load(dir(".env.test"))
+	influxDBUrl, influxDBToken, influxDBDatabase, influxDBSystemTable := os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"), os.Getenv("INFLUXDB_DATABASE"), os.Getenv("INFLUXDB_SYSTEM_TABLE")
+	err := i.Init(influxDBUrl, influxDBToken, influxDBDatabase) // have to make an influxdb3 instance for testing
+	/*
+		install influxdb3 needs to be done manually
+		./influxdb3 serve
+			--node-id=node0 \
+			--cluster-id=cluster0 \
+			--object-store=file \
+			--data-dir=./data
+		install the cli manually
+		use the cli to healthcheck
+
+		init a database (perhaps needing to tear down an existing database from previous test run)
+	*/
+
+	if err != nil {
+		t.Errorf("Could not init database client: %s", err.Error())
+		t.FailNow()
+	}
+
+	// get the current time
+	preWriteTime := time.Now().Format(time.RFC3339Nano)
+
+	// write the arbitrary system points
+	i.WriteArbitrarySystemDatapoint("colour", "grey")
+	i.WriteArbitrarySystemDatapoint("number", 42)
+	i.WriteArbitrarySystemDatapoint("metres", 1.5)
+
+	// sanity testing: check that only the measurements we wrote exist after preWriteTime (cardinality)
+	ctx := context.Background()
+	// idk how this is even queried, this links to the original question, are they all being written to the same table
+	query := fmt.Sprintf("SELECT * FROM %s WHERE time >= TIMESTAMP '%s' ORDER BY time;", influxDBSystemTable, preWriteTime)
+	iterator, err := i.db.Query(ctx, query) // not using public interface of InfluxDB, this is not black box testing but in order to keep the interface of the package consistent it's acceptable
+	if err != nil {
+		t.Errorf("couldn't get query: %s", err.Error())
+		t.FailNow()
+	}
+	var it int = 0
+	for iterator.Next() {
+		// compare the values to what we wrote and ensure that only 3 exist
+		result := iterator.Value()
+		switch it {
+		case 0:
+			compareArbitrarySystemStatusDatapoint("colour", "grey", result)
+		case 1:
+			compareArbitrarySystemStatusDatapoint("number", 42, result)
+		case 2:
+			compareArbitrarySystemStatusDatapoint("metres", 1.5, result)
+		}
+		it++
+	}
+	if it != 3 {
+		t.FailNow()
+	}
+
+}
